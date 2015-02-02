8e5727be71a780eb9f241e2a710f7d1e67ba24b3
aguestuser <guest.austin@gmail.com>
2015-02-02 16:31:32 -0500

    add mutable client collection to Server, add Client class, add client handles

--- src/main/scala/scat/Client.scala
+++ src/main/scala/scat/Client.scala
@@ -1,96 +1,14 @@
-//package scecho
-//
-//import java.net.InetSocketAddress
-//import java.nio.ByteBuffer
-//import java.nio.channels.{AsynchronousServerSocketChannel, AsynchronousSocketChannel, CompletionHandler}
-//
-//import scala.concurrent.duration.Duration
-//import scala.concurrent.{Await, Future, Promise}
-//
-///**
-// * Created by aguestuser on 1/31/15.
-// */
-//
-//object RunClient extends App {
-//  val server = new InetSocketAddress("localhost", 2000)
-//  val client = AsynchronousSocketChannel.open()
-//  Client.run(client,server)
-//}
-//
-//object Client {
-//
-//  def run(client: AsynchronousSocketChannel, server: InetSocketAddress) : Unit = {
-//    val cnxn = connect(client, server)
-//    Await.result(cnxn, Duration.Inf)
-//    cnxn onSuccess { case c => talk(c) }
-//  }
-//
-//  def connect(client: AsynchronousSocketChannel, server: InetSocketAddress ) : Future[AsynchronousSocketChannel] = {
-//    val p = Promise[AsynchronousSocketChannel]()
-//    client.connect(server, null, new CompletionHandler[Void, AsynchronousSocketChannel] {
-//      def completed(att: Void, cnxn: AsynchronousSocketChannel) = {
-//        println(s"Connected to scat server on ${cnxn.getLocalAddress}")
-//        p success { cnxn }
-//      }
-//      def failed(e: Throwable, att: AsynchronousSocketChannel) = { p failure { e } }
-//    })
-//    p.future
-//  }
-//
-//  def talk(cnxn: AsynchronousSocketChannel) : (Future[Unit],Future[Unit]) =
-//    (getMessages(cnxn), getUserInput(cnxn))
-//
-//  def getMessages(cnxn: AsynchronousSocketChannel) : Future[Unit] = {
-//    for {
-//      msg <- readWire(cnxn)
-//      done <- writeToConsole(msg)
-//    } yield getMessages(cnxn)
-//  }
-//
-//  def readWire(cnxn): Future[Array[Byte]] = {
-//    val buf = ByteBuffer.allocate(1024) // TODO what happens to this memory allocation?
-//    val p = Promise[Array[Byte]]()
-//    cnxn.read(buf, null, new CompletionHandler[Integer, Void] {
-//      def completed(numRead: Integer, att: Void) = {
-//        println(s"Read ${numRead.toString} bytes")
-//        buf.flip()
-//        p success { buf.array() }
-//      }
-//      def failed(e: Throwable, att: Void) = p failure { e }
-//    })
-//    p.future
-//  }
-//
-//  def getUserInput(cnxn: AsynchronousSocketChannel) : Future[Unit] =
-//    { for {
-//      msg <- readConsole
-//      done <- writeToWire(msg, cnxn)
-//    } yield done } flatMap { _ => getUserInput(cnxn) }
-//
-//  def readConsole: Future[Array[Byte]] = Future{ scala.io.StdIn.readLine().getBytes }
-//
-//  def writeToWire(msg: Array[Byte], cnxn: AsynchronousSocketChannel): Future[Unit] = {
-//    for {
-//      numWritten <- writeToWireOnce(bs, cnxn)
-//      res <- dispatchWriteToWire(numWritten, bs, cnxn)
-//    } yield res
-//  }
-//
-//  def writeToWireOnce(bs: Array[Byte], chn: AsynchronousSocketChannel): Future[Integer] = {
-//    val p = Promise[Integer]()
-//    chn.write(ByteBuffer.wrap(bs), null, new CompletionHandler[Integer, Void] {
-//      def completed(numWritten: Integer, att: Void) = {
-//        println(s"Echoed ${numWritten.toString} bytes")
-//        p success { numWritten }
-//      }
-//      def failed(e: Throwable, att: Void) = p failure { e }
-//    })
-//    p.future
-//  }
-//
-//  def dispatchWriteToWire(numWritten: Int, bs: Array[Byte], cnxn: AsynchronousSocketChannel): Future[Unit] = {
-//    if(numWritten == bs.size) Future.successful(())
-//    else writeToWire(bs.drop(numWritten), cnxn)
-//  }
-//
-//}
+package scat
+
+import java.nio.channels.AsynchronousSocketChannel
+
+/**
+ * Author: aguestuser
+ * Date: 2/2/15.
+ * License: GPLv2
+ */
+
+class Client(val sock: AsynchronousSocketChannel, val handle: Array[Byte]) {
+  val humanHandle = handle.map{ _.toChar }.mkString
+  val info = humanHandle + " @ " + sock.getRemoteAddress.toString
+}
--- src/main/scala/scat/Server.scala
+++ src/main/scala/scat/Server.scala
@@ -2,23 +2,23 @@ package scat
 
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
-import java.nio.channels.{AsynchronousServerSocketChannel, AsynchronousSocketChannel, CompletionHandler}
+import java.nio.channels._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future, Promise}
 
 /**
- * Author: Austin Guest
+ * Author: @aguestuser
  * Date: 1/31/15
- * License: GPL2
+ * License: GPLv2
  */
 
 object RunServer extends App {
   try {
-    val serverSock = Server.getServerSock(args(0).toInt)
-    val clientSocks = List[AsynchronousSocketChannel]()
-    Server.listenForSocks(serverSock, clientSocks)
+    val sSock = Server.getServerSock(args(0).toInt)
+    println(s"Listening on port ${sSock.getLocalAddress}")
+    Server.accept(sSock)
   } catch {
     case e: NumberFormatException => throw new NumberFormatException("Port number for scat must be a valid int")
   }
@@ -29,44 +29,64 @@ object Server {
   type SSC = AsynchronousServerSocketChannel
   type SC = AsynchronousSocketChannel
 
+  var clients = Set[Client]()
+
   def getServerSock(port: Int): AsynchronousServerSocketChannel =
     AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(port))
 
-  def listenForSocks(sSock: SSC, socks: List[SC]) : Unit = {
-
-    println(s"Listening on port ${sSock.getLocalAddress.toString}")
-    if (socks.size > 0) println(s"Clients at: ${socks map {_.getLocalAddress}}")
-
-    val newSock = acceptSock(sSock)
-    newSock onSuccess { case ns => listenToSocks(ns :: socks)}
-
-    listenForSocks(sSock, Await.result(newSock, Duration.Inf) :: socks )
+  def accept(sSock: SSC) : Unit = {
+    val cSock = acceptOne(sSock)
+    config(cSock)
+    
+    Await.result(cSock,  Duration.Inf)
+    accept(sSock)
   }
 
-  def acceptSock(sSock: SSC): Future[SC] = {
+  def acceptOne(sSock: SSC): Future[SC] = {
     val p = Promise[SC]()
     sSock.accept(null, new CompletionHandler[SC, Void] {
-      def completed(sock: SC, att: Void) = {
-        println(s"Client connection received from ${sock.getRemoteAddress}")
-        p success { sock }
+      def completed(client: SC, att: Void) = {
+        println(s"Client connection received from ${client.getRemoteAddress}")
+        p success { client }
       }
       def failed(e: Throwable, att: Void) = p failure { e }
     })
     p.future
   }
 
-  def listenToSocks(socks: List[SC]): List[Future[List[Unit]]] =
-    socks map { s => listenToSock(s, socks) }
-  
-  def listenToSock(sock: SC, socks: List[SC]) : Future[List[Unit]] =
-    read(sock) flatMap { msg =>
-      relay(msg, sock, socks) flatMap { _ =>
-        listenToSock(sock, socks) } }
+  def config(cSock: Future[SC]) =
+    cSock flatMap { cs =>
+      getHandle(cs) flatMap { h =>
+        val cl = new Client(cs,h)
+        println(s"Created new client: \n${cl.info}")
+        synchronized {
+          clients = clients + cl
+          println(s"${clients.size} total client(s): \n${clients.map{ _.info }.mkString("\n") }")
+        }
+        listen(cl)
+      }
+    }
 
-  def read(sock: SC): Future[Array[Byte]] = {
+  def getHandle(cSock: SC): Future[Array[Byte]] = {
+    val prompt = "Welcome to scat! Please choose a handle...\n".getBytes
+    write(prompt, cSock) flatMap { _ =>
+       read(cSock) map { msg =>
+         trimCarriageReturn(msg)
+       }
+    }
+  }
+
+  def listen(client: Client) : Future[Set[Unit]] =
+    read(client.sock) flatMap { msg =>
+      relay(msg, client)} flatMap { _ =>
+      println(s"Relayed message from ${client.humanHandle}")
+      listen(client)
+    }
+
+  def read(cSock: SC): Future[Array[Byte]] = {
     val buf = ByteBuffer.allocate(1024) // TODO what happens to this memory allocation?
     val p = Promise[Array[Byte]]()
-    sock.read(buf, null, new CompletionHandler[Integer, Void] {
+    cSock.read(buf, null, new CompletionHandler[Integer, Void] {
       def completed(numRead: Integer, att: Void) = {
         println(s"Read $numRead bytes")
         buf.flip()
@@ -77,74 +97,36 @@ object Server {
     p.future
   }
 
-  def relay(msg: Array[Byte], sock: SC, socks: List[SC]): Future[List[Unit]] =
+  def relay(msg: Array[Byte], sender: Client): Future[Set[Unit]] =
     if (msg.map(_.toChar).mkString.trim == "exit") {
-      sock.close()
-      Future.successful(List(())) }
+      sender.sock.close()
+      synchronized { clients = clients - sender }
+      Future.successful(Set(()))
+    }
     else Future sequence {
-      socks map { s =>
-        write(msg, s) } }
-  
-  def write(msg: Array[Byte], sock: SC): Future[Unit] =
-    writeOnce(msg, sock) flatMap { numwrit =>
-      if(numwrit == msg.size) Future.successful(())
-      else write(msg.drop(numwrit), sock)
+      (clients - sender) map { cl => write(appendHandle(msg, sender), cl.sock) }
     }
 
-  def writeOnce(bs: Array[Byte], sock: SC): Future[Integer] = {
+
+  def write(msg: Array[Byte], receiver: SC): Future[Unit] =
+    writeOnce(msg, receiver) flatMap { numwrit =>
+      if (numwrit == msg.size) Future.successful(())
+      else write(msg.drop(numwrit), receiver)
+    }
+  // TODO replace drop with slices of the array? (drop will run in O(n^2) where n is bytes being dropped)
+
+
+  def writeOnce(bs: Array[Byte], receiver: SC): Future[Integer] = {
     val p = Promise[Integer]()
-    sock.write(ByteBuffer.wrap(bs), null, new CompletionHandler[Integer, Void] {
-      def completed(numwrit: Integer, att: Void) = { println(s"Relayed $numwrit bytes"); p success { numwrit } } 
+    receiver.write(ByteBuffer.wrap(bs), null, new CompletionHandler[Integer, Void] {
+      def completed(numwrit: Integer, att: Void) = { println(s"Wrote $numwrit bytes"); p success { numwrit } }
       def failed(e: Throwable, att: Void) = p failure { e }
     })
     p.future
   }
+
+
+  private def appendHandle(msg: Array[Byte], cl: Client) : Array[Byte] = cl.handle ++ ": ".getBytes ++ msg
+  private def trimCarriageReturn(msg: Array[Byte]): Array[Byte] = msg.takeWhile( _ != (10:Byte))
+
 }
-
-
-//  def send(msg: Array[Byte], sock: SC) : Future[Unit] =
-
-
-//  def routeInput(input: Array[Byte], sock: SC, socks: List[SC]) : Future[List[Unit]] = {
-//    if (input.map(_.toChar).mkString.trim == "exit"){ sock.close(); Future.successful(List(())) }
-//    else writeToAllSocks(input,sock, socks)
-//  }
-//
-//  def getMessage(sock: SC) : Future[Array[Byte]] = read(sock)
-
-//  def relayMessage(sock: SC, socks: List[SC]): Future[List[Unit]] = {
-//    { for {
-//      input <- read(sock)
-//      dones <- routeInput(input, sock, socks)
-//    } yield dones } flatMap { _ => relayMessage(sock, socks) }
-//  }
-
-//  def relayMessage(msg: Array[Byte], socks: List[SC])
-
-
-/* stack trace:
-java.nio.channels.ReadPendingException
-  at sun.nio.ch.AsynchronousSocketChannelImpl.read(AsynchronousSocketChannelImpl.java:250)
-  at sun.nio.ch.AsynchronousSocketChannelImpl.read(Asynchrono usSocketChannelImpl.java:296)
-  at java.nio.channels.AsynchronousSocketChannel.read(Asynchr onousSocketChannel.java:407)
-  at scat.Server$.read(Server.scala:71)
-  at scat.Server$.echoOne(Server.scala:62)
-  at scat.Server$$anonfun$echoMany$1.apply(Server.scala:57)
-  at scat.Server$$anonfun$echoMany$1.apply(Server.scala:57)
-  at scala.collection.immutable.List.map(List.scala:272)
-  at scat.Server$.echoMany(Server.scala:57)
-  at scat.Server$$anonfun$echoMany$2.apply(Server.scala:57)
-  at scat.Server$$anonfun$echoMany$2.apply(Server.scala:57)
-  at scala.collection.immutable.List.flatMap(List.scala:327)
-  at scat.Server$.echoMany(Server.scala:57)
-  at scat.Server$$anonfun$listen$1.applyOrElse(Server.scala:3 8)
-  at scat.Server$$anonfun$listen$1.applyOrElse(Server.scala:3 6)
-  at scala.concurrent.Future$$anonfun$onSuccess$1.apply(Futur e.scala:117)
-  at scala.concurrent.Future$$anonfun$onSuccess$1.apply(Futur e.scala:115)
-  at scala.concurrent.impl.CallbackRunnable.run(Promise.scala :32)
-  at scala.concurrent.impl.ExecutionContextImpl$AdaptedForkJo inTask.exec(ExecutionContextImpl.scala:121)
-  at scala.concurrent.forkjoin.ForkJoinTask.doExec(ForkJoinTa sk.java:260)
-  at scala.concurrent.forkjoin.ForkJoinPool$WorkQueue.runTask (ForkJoinPool.java:1339)
-  at scala.concurrent.forkjoin.ForkJoinPool.runWorker(ForkJoi nPool.java:1979)
-  at scala.concurrent.forkjoin.ForkJoinWorkerThread.run(ForkJ oinWorkerThread.java:107)
-* */
