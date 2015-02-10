15cd9a607929786edd3afa8fba40ad187e487276
aguestuser <guest.austin@gmail.com>
2015-02-09 19:12:26 -0500

    Clean up specs, squash bug in Logger retry

--- src/test/scala/scat/Server$Test.scala
+++ src/test/scala/scat/Server$Test.scala
@@ -3,7 +3,6 @@ package scat
 import java.net.InetSocketAddress
 import java.util.Date
 
-import org.specs2.specification.After
 import scat.Client._
 import scat.Server._
 import scat.Socket._
@@ -16,144 +15,187 @@ import scala.concurrent.Promise
 * Date: 2/4/15.
 * License: GPLv2
 */
+
+
 class Server$Test extends org.specs2.mutable.Specification {
+
+  import scat.Helpers._
+  val sWait = 20
+  val cWait = 60
 
   "Server" should {
 
-    lazy val sAddr: SAddr = new InetSocketAddress(2000)
-    lazy val ks = Promise[Unit]()
-    lazy val server = Server(getServerSock(2000), CMap[RmtClient, Boolean](), CMap[Date, String](), ks)
+    "accept a client connection" in {
 
-    lazy val (cSock1, h1, kc1) = (getClientSock(2001), "austin".getBytes, Promise[Unit]())
-    lazy val c1 = CfgClient(cSock1, h1)
-
-    lazy val (cSock2, h2, kc2) = (getClientSock(2002), "friend".getBytes, Promise[Unit]())
-    lazy val c2 = CfgClient(cSock2, h2)
-
-    "accept a client connection" in new closeSocketsAfter {
+      val (sPort, cPort) = (2000,2001)
+      val (server, sAddr) = getServer(sPort)
+      val (c, kc) = getClient(cPort, "austin")
 
       acceptClients(server)
-      Thread.sleep(5)
+      Thread.sleep(sWait)
 
-      connectToServer(c1,sAddr,kc1)
-      Thread.sleep(20)
+      connectToServer(c,sAddr,kc)
+      Thread.sleep(cWait)
 
       getLogs(server.logs, 1).mkString("\n") ===
-        "Client connection received from /0:0:0:0:0:0:0:1:2001"
+        s"Client connection received from /0:0:0:0:0:0:0:1:$cPort"
 
     }
 
-    "configure a client object" in new closeSocketsAfter {
+    "configure a client object" in {
+
+      val (sPort, cPort) = (2002,2003)
+      val (server, sAddr) = getServer(sPort)
+      val (c, kc) = getClient(cPort, "austin")
 
       acceptClients(server)
-      Thread.sleep(5)
+      Thread.sleep(sWait)
 
-      connectToServer(c1,sAddr,kc1) flatMap { cnctCl =>
+      connectToServer(c,sAddr,kc) flatMap { cnctCl =>
         Client.shakeHands(cnctCl) }
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       getLogs(server.logs, 2).mkString("\n") ===
-        "Client connection received from /0:0:0:0:0:0:0:1:2001\n" +
+        s"Client connection received from /0:0:0:0:0:0:0:1:$cPort\n" +
           "Created new client: \n" +
-          "austin @ /0:0:0:0:0:0:0:1:2001\n" +
+          s"austin @ /0:0:0:0:0:0:0:1:$cPort\n" +
           "1 total client(s): \n" +
-          "austin @ /0:0:0:0:0:0:0:1:2001\n"
+          s"austin @ /0:0:0:0:0:0:0:1:$cPort"
     }
 
-    "configure two client objects" in new closeSocketsAfter {
+    "configure two client objects" in  {
+
+      val (sPort, c1Port, c2Port) = (2004,2005,2006)
+      val (server, sAddr) = getServer(sPort)
+      val (c1, kc1) = getClient(c1Port, "austin")
+      val (c2, kc2) = getClient(c2Port, "friend")
 
       acceptClients(server)
-      Thread.sleep(5)
+      Thread.sleep(sWait)
 
       connectToServer(c1,sAddr,kc1) flatMap { cnctCl =>
         Client.shakeHands(cnctCl) }
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       connectToServer(c2,sAddr,kc2) flatMap { cnctCl =>
         Client.shakeHands(cnctCl) }
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       getLogs(server.logs, 4).mkString("\n") ===
-        "Client connection received from /0:0:0:0:0:0:0:1:2001\n" +
+        s"Client connection received from /0:0:0:0:0:0:0:1:$c1Port\n" +
           "Created new client: \n" +
-          "austin @ /0:0:0:0:0:0:0:1:2001\n" +
+          s"austin @ /0:0:0:0:0:0:0:1:$c1Port\n" +
           "1 total client(s): \n" +
-          "austin @ /0:0:0:0:0:0:0:1:2001\n" +
-          "Client connection received from /0:0:0:0:0:0:0:1:2002\n" +
+          s"austin @ /0:0:0:0:0:0:0:1:$c1Port\n" +
+          s"Client connection received from /0:0:0:0:0:0:0:1:$c2Port\n" +
           "Created new client: \n" +
-          "friend @ /0:0:0:0:0:0:0:1:2002\n" +
+          s"friend @ /0:0:0:0:0:0:0:1:$c2Port\n" +
           "2 total client(s): \n" +
-          "austin @ /0:0:0:0:0:0:0:1:2001\n" +
-          "friend @ /0:0:0:0:0:0:0:1:2002"
+          s"austin @ /0:0:0:0:0:0:0:1:$c1Port\n" +
+          s"friend @ /0:0:0:0:0:0:0:1:$c2Port"
     }
 
-    "send messages between clients" in new closeSocketsAfter {
+    "send messages between clients" in {
+
+      val (sPort, c1Port, c2Port) = (2007,2008,2009)
+      val (server, sAddr) = getServer(sPort)
+      val (c1, kc1) = getClient(c1Port, "austin")
+      val (c2, kc2) = getClient(c2Port, "friend")
 
       acceptClients(server)
-      Thread.sleep(5)
+      Thread.sleep(sWait)
 
       connectToServer(c1,sAddr,kc1) flatMap { cnctCl =>
         Client.shakeHands(cnctCl) }
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       connectToServer(c2,sAddr,kc2) flatMap { cnctCl =>
         Client.shakeHands(cnctCl) }
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       val (cc1, cc2) = (CnctClient(c1.sock,c1.handle), CnctClient(c2.sock,c2.handle))
       Client.dispatchOut(cc1, "hi friend!".getBytes, kc1)
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       Client.dispatchOut(cc2, "hi austin!".getBytes, kc2)
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       getLogs(server.logs, 2).mkString("\n") ===
-        "Relayed message from austin @ /0:0:0:0:0:0:0:1:2001\n" +
+        "Relayed message from austin @ /0:0:0:0:0:0:0:1:2008\n" +
           "austin: hi friend!\n" +
-          "Relayed message from friend @ /0:0:0:0:0:0:0:1:2002\n" +
+          "Relayed message from friend @ /0:0:0:0:0:0:0:1:2009\n" +
           "friend: hi austin!"
     }
 
-    "process client exits" in new closeSocketsAfter {
+    "process client exits" in {
+
+      val (sPort, c1Port, c2Port) = (2010,2011,2012)
+      val (server, sAddr) = getServer(sPort)
+      val (c1, kc1) = getClient(c1Port, "austin")
+      val (c2, kc2) = getClient(c2Port, "friend")
 
       acceptClients(server)
-      Thread.sleep(5)
+      Thread.sleep(sWait)
 
       connectToServer(c1,sAddr,kc1) flatMap { cnctCl =>
         Client.shakeHands(cnctCl) }
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       connectToServer(c2,sAddr,kc2) flatMap { cnctCl =>
         Client.shakeHands(cnctCl) }
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       val (cc1, cc2) = (CnctClient(c1.sock,c1.handle), CnctClient(c2.sock,c2.handle))
       Client.dispatchOut(cc1, "hi friend!".getBytes, kc1)
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       Client.dispatchOut(cc2, "hi austin!".getBytes, kc2)
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       Client.dispatchOut(cc1, "exit".getBytes, kc1)
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       Client.dispatchOut(cc2, "exit".getBytes, kc2)
-      Thread.sleep(20)
+      Thread.sleep(cWait)
 
       getLogs(server.logs, 2).mkString("\n") ===
-        "Closed connection with austin @ /0:0:0:0:0:0:0:1:2001\n" +
-          "Closed connection with friend @ /0:0:0:0:0:0:0:1:2002"
-    }
-
-    trait closeSocketsAfter extends After {
-      //TODO figure out setup / teardown
-      def after = {
-        server.sSock.close()
-        c1.sock.close()
-        c2.sock.close()
-      }
+        "Closed connection with austin @ /0:0:0:0:0:0:0:1:2011\n" +
+          "Closed connection with friend @ /0:0:0:0:0:0:0:1:2012"
     }
   }
 }
 
 
+object Helpers {
+
+  def getPorts(ports: Stream[Int], num: Int): List[Int] =
+  ports.take(num).toList
+
+  def getServer(sPort: Int): (Server, InetSocketAddress) = {
+    val sAddr: SAddr = new InetSocketAddress(sPort)
+    val ks = Promise[Unit]()
+    val server = Server(getServerSock(sPort), CMap[RmtClient, Boolean](), CMap[Date, String](), ks)
+    (server, sAddr) }
+
+  def getClient(cPort: Int, handle: String): (CfgClient, Promise[Unit]) =
+  (CfgClient(getClientSock(cPort), handle.getBytes), Promise[Unit]())
+
+}
+
+//class serverWithOneClient(_sPort: Int, _cPort: Int) extends org.specs2.mutable.After {
+//  import scat.Helpers._
+//
+//val ports = Stream from 2000
+//  val sWait = 100
+//  val cWait = 100
+//
+//  val (sPort, cPort) = (_sPort, _cPort)
+//  val (server, sAddr) = getServer(sPort)
+//  val (c, kc) = getClient(cPort, "austin")
+//
+//  def after: Future[Unit] = {
+//    Server.doKill(server) flatMap { _ =>
+//      Future successful { c.sock.close() }
+//    }
+//  }
+//}
